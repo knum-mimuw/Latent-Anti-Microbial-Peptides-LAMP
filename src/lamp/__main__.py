34442babe28ac863a218be718fae9d8125e2eff4
pszmk <przemyslawpietrzak2001@gmail.com>
2025-10-05 22:00:40 +0200

    rm old stuff

--- src/lamp/__main__.py
+++
@@ -1,9 +0,0 @@
-from pytorch_lightning.cli import LightningCLI
-
-
-def main() -> None:
-    LightningCLI(run=True, subclass_mode_model=True, subclass_mode_data=True)
-
-
-if __name__ == "__main__":
-    main()
--- src/lamp/datamodules/utils.py
+++
@@ -1,5 +0,0 @@
-import gdown
-
-link = "https://drive.google.com/drive/folders/1krim1ugqNDmgmHZCFSOvmynWxCSzyOto"
-target_dir = "downloaded_data_zips"
-gdown.download_folder(link, output=target_dir)
